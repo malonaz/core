f9b1ba19b3d5372f652f2274b0f6001a0c9f8466
Malon Azria <malonazria@gmail.com>
2025-10-15 16:20:34 +0200

    generate all codegen

--- cmd/copy_generated_code.sh
+++ cmd/copy_generated_code.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 mkdir -p genproto/grpc && cp -rf plz-out/gen/proto/grpc.pb.go genproto/grpc/
 mkdir -p genproto/authentication && cp -rf plz-out/gen/proto/authentication.pb.go genproto/authentication/
+mkdir -p genproto/codegen/model && cp -rf plz-out/gen/proto/codegen/model.pb.go genproto/codegen/model
+mkdir -p genproto/codegen/admin_api && cp -rf plz-out/gen/proto/codegen/admin_api.pb.go genproto/codegen/admin_api
+mkdir -p genproto/codegen/rpc && cp -rf plz-out/gen/proto/codegen/rpc.pb.go genproto/codegen/rpc
+mkdir -p genproto/codegen/aip && cp -rf plz-out/gen/proto/codegen/aip.pb.go genproto/codegen/aip
