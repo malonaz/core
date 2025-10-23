1502fd8971fe9c2b7a791b744c98ea696bd34aa9
Malon Azria <malonazria@gmail.com>
2025-10-23 13:42:24 +0200

    rename admin_api to gateawy

--- cmd/copy_generated_code.sh
+++ cmd/copy_generated_code.sh
@@ -2,6 +2,6 @@
 mkdir -p genproto/grpc && cp -rf plz-out/gen/proto/grpc.pb.go genproto/grpc/
 mkdir -p genproto/authentication && cp -rf plz-out/gen/proto/authentication.pb.go genproto/authentication/
 mkdir -p genproto/codegen/model && cp -rf plz-out/gen/proto/codegen/model.pb.go genproto/codegen/model
-mkdir -p genproto/codegen/admin_api && cp -rf plz-out/gen/proto/codegen/admin_api.pb.go genproto/codegen/admin_api
+mkdir -p genproto/codegen/gateway && cp -rf plz-out/gen/proto/codegen/gateway.pb.go genproto/codegen/gateway
 mkdir -p genproto/codegen/rpc && cp -rf plz-out/gen/proto/codegen/rpc.pb.go genproto/codegen/rpc
 mkdir -p genproto/codegen/aip && cp -rf plz-out/gen/proto/codegen/aip.pb.go genproto/codegen/aip
