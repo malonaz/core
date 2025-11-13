9dcb9b992039b999b97d3281b2ea24cd8e727dd6
Malon Azria <malonazria@gmail.com>
2025-11-13 08:53:38 +0100

    [proto/codegen] - delete rpc.proto

--- cmd/copy_generated_code.sh
+++ cmd/copy_generated_code.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+rm -rf genproto
 mkdir -p genproto/grpc && cp -rf plz-out/gen/proto/grpc.pb.go genproto/grpc/
 mkdir -p genproto/authentication && cp -rf plz-out/gen/proto/authentication.pb.go genproto/authentication/
 mkdir -p genproto/codegen/model && cp -rf plz-out/gen/proto/codegen/model.pb.go genproto/codegen/model
@@ -8,3 +9,6 @@ mkdir -p genproto/codegen/aip && cp -rf plz-out/gen/proto/codegen/aip.pb.go genp
 
 # Test files.
 mkdir -p genproto/test/aip && cp -rf plz-out/gen/proto/test/aip.pb.go genproto/test/aip
+
+# Regenerate the BUILD files.
+plz lint
