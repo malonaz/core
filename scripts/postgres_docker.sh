724090111c19229f4a0a8362123783eb2a42905e
Malon Azria <malonazria@gmail.com>
2025-12-30 19:26:53 +0100

    [scripts/postgres] - add db name arg for connect

--- scripts/postgres_docker.sh
+++ scripts/postgres_docker.sh
@@ -8,6 +8,7 @@ NC='\033[0m'
 
 HOST_PORT=""
 NAME=""
+POSITIONAL_ARGS=()
 
 while [[ $# -gt 0 ]]; do
   case "$1" in
@@ -20,28 +21,29 @@ while [[ $# -gt 0 ]]; do
       shift 2
       ;;
     *)
-      ACTION="$1"
+      POSITIONAL_ARGS+=("$1")
       shift
       ;;
   esac
 done
 
+ACTION="${POSITIONAL_ARGS[0]}"
+
 if [[ -z "$NAME" ]]; then
   echo -e "${RED}Error: --name is required${NC}"
-  echo "Usage: $0 {start|stop|reset|connect} --name NAME --port PORT"
+  echo "Usage: $0 {start|stop|reset|connect [db_name]} --name NAME --port PORT"
   exit 1
 fi
 
 if [[ -z "$HOST_PORT" ]]; then
   echo -e "${RED}Error: --port is required${NC}"
-  echo "Usage: $0 {start|stop|reset|connect} --name NAME --port PORT"
+  echo "Usage: $0 {start|stop|reset|connect [db_name]} --name NAME --port PORT"
   exit 1
 fi
 
 CONTAINER_NAME="postgres_${NAME}_${HOST_PORT}"
 POSTGRES_PASSWORD="postgres"
 POSTGRES_USER="postgres"
-POSTGRES_DB="postgres"
 CONTAINER_PORT="5432"
 
 case "$ACTION" in
@@ -51,13 +53,13 @@ case "$ACTION" in
       --name $CONTAINER_NAME \
       -e POSTGRES_PASSWORD=$POSTGRES_PASSWORD \
       -e POSTGRES_USER=$POSTGRES_USER \
-      -e POSTGRES_DB=$POSTGRES_DB \
+      -e POSTGRES_DB=postgres \
       -p $HOST_PORT:$CONTAINER_PORT \
       postgres:latest >/dev/null
 
     if [ $? -eq 0 ]; then
       echo -e "${GREEN}PostgreSQL '$CONTAINER_NAME' started successfully on port $HOST_PORT${NC}"
-      echo -e "${YELLOW}Connection string:${NC} postgresql://$POSTGRES_USER:$POSTGRES_PASSWORD@localhost:$HOST_PORT/$POSTGRES_DB"
+      echo -e "${YELLOW}Connection string:${NC} postgresql://$POSTGRES_USER:$POSTGRES_PASSWORD@localhost:$HOST_PORT/postgres"
     else
       echo -e "${RED}Failed to start PostgreSQL container '$CONTAINER_NAME'${NC}"
       exit 1
@@ -78,12 +80,13 @@ case "$ACTION" in
     ;;
 
   connect)
-    echo -e "${BLUE}Connecting to PostgreSQL database '$CONTAINER_NAME'...${NC}"
-    PGPASSWORD=$POSTGRES_PASSWORD psql -h localhost -p $HOST_PORT -U $POSTGRES_USER -d $POSTGRES_DB
+    DB_NAME="${POSITIONAL_ARGS[1]:-postgres}"
+    echo -e "${BLUE}Connecting to PostgreSQL database '$DB_NAME'...${NC}"
+    PGPASSWORD=$POSTGRES_PASSWORD psql -h localhost -p $HOST_PORT -U $POSTGRES_USER -d "$DB_NAME"
     ;;
 
   *)
-    echo "Usage: $0 {start|stop|reset|connect} --name NAME --port PORT"
+    echo "Usage: $0 {start|stop|reset|connect [db_name]} --name NAME --port PORT"
     exit 1
     ;;
 esac
