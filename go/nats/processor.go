3ff97bc904f5c4a0674a6a192bfdf178b7642ec9
Malon <malonazria@gmail.com>
2026-03-03 23:04:49 +0100

    [go/nats] - expose Payloads() iteraotr

--- go/nats/processor.go
+++ go/nats/processor.go
@@ -199,3 +199,13 @@ func (p *Processor[T]) Close() {
 		p.routine.Close()
 	}
 }
+
+func Payloads[T proto.Message](messages []*Message[T]) func(func(int, T) bool) {
+	return func(yield func(int, T) bool) {
+		for i, message := range messages {
+			if !yield(i, message.Payload) {
+				return
+			}
+		}
+	}
+}
