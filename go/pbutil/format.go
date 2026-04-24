8b66b18f751ea726c9cd22b7dead3d8aa87b43e7
Malon Azria <malonazria@gmail.com>
2026-04-24 14:25:53 +0200

    [go/pbutil] - more enum formats

--- go/pbutil/format.go
+++ go/pbutil/format.go
@@ -1,16 +1,30 @@
 package pbutil
 
 import (
-  "strings"
+	"strings"
 
-  "github.com/iancoleman/strcase"
-  "google.golang.org/protobuf/reflect/protoreflect"
+	"github.com/iancoleman/strcase"
+	"golang.org/x/text/cases"
+	"golang.org/x/text/language"
+	"google.golang.org/protobuf/reflect/protoreflect"
 )
 
 func TrimEnumPrefix(enum protoreflect.Enum) string {
-  descriptor := enum.Descriptor()
-  value := descriptor.Values().ByNumber(enum.Number())
-  name := string(value.Name())
-  prefix := strcase.ToScreamingSnake(string(descriptor.Name())) + "_"
-  return strings.TrimPrefix(name, prefix)
+	descriptor := enum.Descriptor()
+	value := descriptor.Values().ByNumber(enum.Number())
+	name := string(value.Name())
+	prefix := strcase.ToScreamingSnake(string(descriptor.Name())) + "_"
+	return strings.TrimPrefix(name, prefix)
+}
+
+func FormatEnumKebab(enum protoreflect.Enum) string {
+	return strcase.ToKebab(TrimEnumPrefix(enum))
+}
+
+func FormatEnumSnake(enum protoreflect.Enum) string {
+	return strcase.ToSnake(TrimEnumPrefix(enum))
+}
+
+func FormatEnumTitle(enum protoreflect.Enum) string {
+	return cases.Title(language.English).String(strcase.ToDelimited(TrimEnumPrefix(enum), ' '))
 }
--- go/pbutil/format_test.go
+++ go/pbutil/format_test.go
@@ -1,31 +1,85 @@
 package pbutil
 
 import (
-  "testing"
+	"testing"
 
-  "github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/require"
 
-  librarypb "github.com/malonaz/core/genproto/test/library/v1"
+	librarypb "github.com/malonaz/core/genproto/test/library/v1"
 )
 
 func TestTrimEnumPrefix(t *testing.T) {
-  tests := []struct {
-    name     string
-    enum     librarypb.ShelfGenre
-    expected string
-  }{
-    {"Unspecified", librarypb.ShelfGenre_SHELF_GENRE_UNSPECIFIED, "UNSPECIFIED"},
-    {"Fiction", librarypb.ShelfGenre_SHELF_GENRE_FICTION, "FICTION"},
-    {"History", librarypb.ShelfGenre_SHELF_GENRE_HISTORY, "HISTORY"},
-    {"Biography", librarypb.ShelfGenre_SHELF_GENRE_BIOGRAPHY, "BIOGRAPHY"},
-    {"ScienceFiction", librarypb.ShelfGenre_SHELF_GENRE_SCIENCE_FICTION, "SCIENCE_FICTION"},
-    {"NonFiction", librarypb.ShelfGenre_SHELF_GENRE_NON_FICTION, "NON_FICTION"},
-  }
+	tests := []struct {
+		name     string
+		enum     librarypb.ShelfGenre
+		expected string
+	}{
+		{"Unspecified", librarypb.ShelfGenre_SHELF_GENRE_UNSPECIFIED, "UNSPECIFIED"},
+		{"Fiction", librarypb.ShelfGenre_SHELF_GENRE_FICTION, "FICTION"},
+		{"History", librarypb.ShelfGenre_SHELF_GENRE_HISTORY, "HISTORY"},
+		{"Biography", librarypb.ShelfGenre_SHELF_GENRE_BIOGRAPHY, "BIOGRAPHY"},
+		{"ScienceFiction", librarypb.ShelfGenre_SHELF_GENRE_SCIENCE_FICTION, "SCIENCE_FICTION"},
+		{"NonFiction", librarypb.ShelfGenre_SHELF_GENRE_NON_FICTION, "NON_FICTION"},
+	}
 
-  for _, tc := range tests {
-    t.Run(tc.name, func(t *testing.T) {
-      result := TrimEnumPrefix(tc.enum)
-      require.Equal(t, tc.expected, result)
-    })
-  }
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			result := TrimEnumPrefix(tc.enum)
+			require.Equal(t, tc.expected, result)
+		})
+	}
+}
+
+func TestFormatEnumKebab(t *testing.T) {
+	tests := []struct {
+		name     string
+		enum     librarypb.ShelfGenre
+		expected string
+	}{
+		{"Fiction", librarypb.ShelfGenre_SHELF_GENRE_FICTION, "fiction"},
+		{"ScienceFiction", librarypb.ShelfGenre_SHELF_GENRE_SCIENCE_FICTION, "science-fiction"},
+		{"NonFiction", librarypb.ShelfGenre_SHELF_GENRE_NON_FICTION, "non-fiction"},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			require.Equal(t, tc.expected, FormatEnumKebab(tc.enum))
+		})
+	}
+}
+
+func TestFormatEnumSnake(t *testing.T) {
+	tests := []struct {
+		name     string
+		enum     librarypb.ShelfGenre
+		expected string
+	}{
+		{"Fiction", librarypb.ShelfGenre_SHELF_GENRE_FICTION, "fiction"},
+		{"ScienceFiction", librarypb.ShelfGenre_SHELF_GENRE_SCIENCE_FICTION, "science_fiction"},
+		{"NonFiction", librarypb.ShelfGenre_SHELF_GENRE_NON_FICTION, "non_fiction"},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			require.Equal(t, tc.expected, FormatEnumSnake(tc.enum))
+		})
+	}
+}
+
+func TestFormatEnumTitle(t *testing.T) {
+	tests := []struct {
+		name     string
+		enum     librarypb.ShelfGenre
+		expected string
+	}{
+		{"Fiction", librarypb.ShelfGenre_SHELF_GENRE_FICTION, "Fiction"},
+		{"ScienceFiction", librarypb.ShelfGenre_SHELF_GENRE_SCIENCE_FICTION, "Science Fiction"},
+		{"NonFiction", librarypb.ShelfGenre_SHELF_GENRE_NON_FICTION, "Non Fiction"},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			require.Equal(t, tc.expected, FormatEnumTitle(tc.enum))
+		})
+	}
 }
