231d2d17adde970d959bfc9c27b8085e87a6940a
Malon Azria <malonazria@gmail.com>
2026-05-27 20:02:53 +0200

    [go/aip] - added deterministic resource id

--- go/aip/resource_id.go
+++ go/aip/resource_id.go
@@ -19,3 +19,10 @@ func NewSystemGeneratedBase32ResourceID() string {
 	id := uuid.MustNewV7()
 	return base32Encoding.EncodeToString(id[:])
 }
+
+// NewDeterministicBase32ResourceID returns a deterministic base32-encoded resource ID
+// derived from a namespace UUID and a name string using UUIDv5.
+func NewDeterministicBase32ResourceID(namespace uuid.UUID, name string) string {
+	id := uuid.NewV5(namespace, name)
+	return base32Encoding.EncodeToString(id[:])
+}
