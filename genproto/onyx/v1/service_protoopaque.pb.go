aaaa7b1bd6910d783543626b62025ac61e8c3f70
Malon Azria <malonazria@gmail.com>
2026-09-10 18:08:22 +0200

    [tools/onyx] - name grpc clients, reject same-named clients on different protos (#49)
    Two grpc_client deps sharing a service name on different protos silently collapsed into one variable; the collision check compared identical owner strings. Adds an optional name to grpc_client used for identifiers.

--- genproto/onyx/v1/service_protoopaque.pb.go
+++ genproto/onyx/v1/service_protoopaque.pb.go
@@ -735,6 +735,7 @@ type Dependency_GrpcClient struct {
 	state              protoimpl.MessageState `protogen:"opaque.v1"`
 	xxx_hidden_Proto   string                 `protobuf:"bytes,1,opt,name=proto,proto3"`
 	xxx_hidden_Service string                 `protobuf:"bytes,2,opt,name=service,proto3"`
+	xxx_hidden_Name    string                 `protobuf:"bytes,3,opt,name=name,proto3"`
 	unknownFields      protoimpl.UnknownFields
 	sizeCache          protoimpl.SizeCache
 }
@@ -778,12 +779,23 @@ func (x *Dependency_GrpcClient) GetService() string {
 	return ""
 }
 
+func (x *Dependency_GrpcClient) GetName() string {
+	if x != nil {
+		return x.xxx_hidden_Name
+	}
+	return ""
+}
+
 func (x *Dependency_GrpcClient) SetProto(v string) {
 	x.xxx_hidden_Proto = v
 }
 
 func (x *Dependency_GrpcClient) SetService(v string) {
 	x.xxx_hidden_Service = v
+}
+
+func (x *Dependency_GrpcClient) SetName(v string) {
+	x.xxx_hidden_Name = v
 }
 
 type Dependency_GrpcClient_builder struct {
@@ -793,6 +805,10 @@ type Dependency_GrpcClient_builder struct {
 	Proto string
 	// Kebab-case name of the proto service.
 	Service string
+	// Kebab-case name of the client: the constructor argument and, for a remote client, its
+	// `{name}-grpc` flags. Defaults to service; set it when a binary hosts two services of the
+	// same name on different protos.
+	Name string
 }
 
 func (b0 Dependency_GrpcClient_builder) Build() *Dependency_GrpcClient {
@@ -801,6 +817,7 @@ func (b0 Dependency_GrpcClient_builder) Build() *Dependency_GrpcClient {
 	_, _ = b, x
 	x.xxx_hidden_Proto = b.Proto
 	x.xxx_hidden_Service = b.Service
+	x.xxx_hidden_Name = b.Name
 	return m0
 }
 
@@ -1105,7 +1122,7 @@ const file_malonaz_onyx_v1_service_proto_rawDesc = "" +
 	"\x06target\x18\x03 \x01(\tB\a\xbaH\x04r\x02\x10\x01R\x06target\x12\x12\n" +
 	"\x04nats\x18\x04 \x01(\bR\x04nats\x12 \n" +
 	"\vlongrunning\x18\x05 \x01(\bR\vlongrunningB\r\n" +
-	"\x04kind\x12\x05\xbaH\x02\b\x01\"\xeb\x06\n" +
+	"\x04kind\x12\x05\xbaH\x02\b\x01\"\xa4\a\n" +
 	"\n" +
 	"Dependency\x12I\n" +
 	"\vgrpc_client\x18\x01 \x01(\v2&.malonaz.onyx.v1.Dependency.GrpcClientH\x00R\n" +
@@ -1113,11 +1130,12 @@ const file_malonaz_onyx_v1_service_proto_rawDesc = "" +
 	"\x12postgres_db_client\x18\x02 \x01(\v2,.malonaz.onyx.v1.Dependency.PostgresDbClientH\x00R\x10postgresDbClient\x12U\n" +
 	"\x0fpostgres_client\x18\x03 \x01(\v2*.malonaz.onyx.v1.Dependency.PostgresClientH\x00R\x0epostgresClient\x126\n" +
 	"\x04nats\x18\x04 \x01(\v2 .malonaz.onyx.v1.Dependency.NatsH\x00R\x04nats\x12?\n" +
-	"\aservice\x18\x05 \x01(\v2#.malonaz.onyx.v1.Dependency.ServiceH\x00R\aservice\x1af\n" +
+	"\aservice\x18\x05 \x01(\v2#.malonaz.onyx.v1.Dependency.ServiceH\x00R\aservice\x1a\x9e\x01\n" +
 	"\n" +
 	"GrpcClient\x12\x1d\n" +
 	"\x05proto\x18\x01 \x01(\tB\a\xbaH\x04r\x02\x10\x01R\x05proto\x129\n" +
-	"\aservice\x18\x02 \x01(\tB\x1f\xbaH\x1cr\x1a2\x18^[a-z0-9]+(-[a-z0-9]+)*$R\aservice\x1a\x92\x01\n" +
+	"\aservice\x18\x02 \x01(\tB\x1f\xbaH\x1cr\x1a2\x18^[a-z0-9]+(-[a-z0-9]+)*$R\aservice\x126\n" +
+	"\x04name\x18\x03 \x01(\tB\"\xbaH\x1f\xd8\x01\x01r\x1a2\x18^[a-z0-9]+(-[a-z0-9]+)*$R\x04name\x1a\x92\x01\n" +
 	"\x10PostgresDbClient\x12(\n" +
 	"\x04name\x18\x01 \x01(\tB\x14\xbaH\x11r\x0f2\r^[a-z0-9_-]+$R\x04name\x123\n" +
 	"\bdatabase\x18\x02 \x01(\tB\x17\xbaH\x14\xd8\x01\x01r\x0f2\r^[a-z0-9_-]+$R\bdatabase\x12\x1f\n" +
